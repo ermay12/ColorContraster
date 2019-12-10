f3eeb3a89d7ea0fb0402d3b07cc7fec869abba90
Eric Maynard <ermay12@gmail.com>
2019-12-10 14:37:26 -0500

    Updates

---
+++ precache-manifest.9794397a4c4c41979d830b72f9e2cc3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ad0e1ed66be8c55e9d2dba9c1dd14d",
+    "url": "/ColorContraster/index.html"
+  },
+  {
+    "revision": "9c220c13263bf7823d3e",
+    "url": "/ColorContraster/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6d6d7369f84961b9c004",
+    "url": "/ColorContraster/static/js/2.d28f9c89.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/ColorContraster/static/js/2.d28f9c89.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c220c13263bf7823d3e",
+    "url": "/ColorContraster/static/js/main.829342a1.chunk.js"
+  },
+  {
+    "revision": "28ecfaa9d3ff4eeeb9db",
+    "url": "/ColorContraster/static/js/runtime-main.9d956812.js"
+  }
+]);
